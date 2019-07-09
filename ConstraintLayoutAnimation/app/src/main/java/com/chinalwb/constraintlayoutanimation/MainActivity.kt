20bf212297c24775ad5e7c19bc29b0bdfa51b7e3
Rain Liu <liuwenbin168@126.com>
2019-07-09 23:36:43 +0800

    for sample

---
+++ ConstraintLayoutAnimation/app/src/main/java/com/chinalwb/constraintlayoutanimation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.chinalwb.constraintlayoutanimation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    var constraintLayout: ConstraintLayout? = null
+    var tap: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        constraintLayout = findViewById(R.id.constraint)
+        tap = findViewById(R.id.tap)
+        tap!!.setOnClickListener { animateToKeyframeTwo() }
+    }
+
+    fun animateToKeyframeTwo() {
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this, R.layout.activity_main)
+        constraintSet.clone(this, R.layout.keyframe_2)
+
+        val transition = ChangeBounds()
+        transition.interpolator = AnticipateOvershootInterpolator(1.0F)
+        transition.duration = 1200
+
+        TransitionManager.beginDelayedTransition(constraintLayout, transition)
+        constraintSet.applyTo(constraintLayout)
+    }
+}
